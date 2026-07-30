2bc90a17752078e2ea00a532246825192060c070
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-07-30 15:58:13 +0800

    Improve permutation generation in permutation.py
    Refactor permutation generation logic and fix output formatting.

--- cses/permutation.py
+++ cses/permutation.py
@@ -1,7 +1,7 @@
 import math
 top: int = int(input(""))
 
-con: str = ""
+con = None
 if top == 1:
     print(1)
 elif top < 3:
@@ -10,12 +10,14 @@ elif top == 4:
     print("3 1 4 2")
 else:
     diff = math.ceil(top/2)
-    for i in range(1, diff):
-        con += f"{str(i)} {str(i+diff)} "
+    con = [(i, i+diff) for i in range(1, diff)]
         
     if top % 2 == 0:
-        con += f"{diff} {top}"
+        con.append((diff, top))
     else:
-        con += f"{diff}"
+        con.append(diff)
+    
+    con = str(con)
+    con = con.replace(")","").replace("(","").replace(", "," ").replace("]","").replace("[","")
         
 print(con)
