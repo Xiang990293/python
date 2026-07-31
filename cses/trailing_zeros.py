2106ff6a57ff21f1dc8ff1e073be067210a51d90
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-07-31 12:38:42 +0800

    Refactor trailing zeros calculation with else clause

--- cses/trailing_zeros.py
+++ cses/trailing_zeros.py
@@ -2,11 +2,11 @@ n = int(input())
 
 if n < 5:
     print(0)
-
-i = 5
-result = 0
-while i <= n:
-    result += n//i
-    i *= 5
-    
-print(result)
+else:
+    i = 5
+    result = 0
+    while i <= n:
+        result += n//i
+        i *= 5
+        
+    print(result)
