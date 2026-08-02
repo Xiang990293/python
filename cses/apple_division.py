988eb5e1abeee67c1196312de7c6085be31980a4
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-02 10:40:28 +0800

    Add solution for apple division problem
    Implement apple division problem solution with sorting and nearest value calculation.

---
+++ cses/apple_division.py
@@ -0,0 +1,69 @@
+n = int(input())
+a = list(map(int, input().split(" ")))
+b = []
+prevres, res = None, None
+
+def nearest(l, f):
+    leng = len(l)
+    
+    if leng == 1:
+        return 0, l[0]
+        
+    mid = leng//2
+    
+    
+    
+    if f == l[mid]:
+        return mid, f
+    
+    if f > l[mid]:
+        delta, val = nearest(l[mid:], f)
+        return mid + delta, val
+    if f < l[mid]:
+        delta, val =  nearest(l[:mid], f)
+        return delta, val
+
+while True:
+    sa = sum(a)
+    if b == []:
+        sb = 0
+    else:
+        sb = sum(b)
+    if sa < sb:
+        a, b = b, a
+        sa, sb = sb, sa
+        
+    a.sort()
+    b.sort()
+    
+    dsab = (sa - sb)
+    fhdsab = dsab / 2
+    hdsab = dsab//2
+    if sa == sb:
+        res = 0
+        break
+    
+    
+    prevres = res
+    sind, _ = nearest(a, fhdsab)
+    for i in a[:sind+1][::-1]:
+        res = fhdsab - i
+        if res < 0:
+            break
+        if i in a and prevres is None:
+            b.append(i)
+            a.remove(i)
+            break
+            
+        elif i in a and prevres > res:
+            b.append(i)
+            a.remove(i)
+            break
+        
+    print(a, b, res, prevres)
+    if prevres == res and res is not None:
+        break
+    
+    
+print(a, b, res, prevres, sa - sb)
+
