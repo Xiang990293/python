0f20cdc840b9ae3b282928bd0a653ee012fd76ad
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-03 11:30:13 +0800

    Refactor chessboard input and board representation, and improve performance with dealing numbers

--- cses/chessboard_and_queens.py
+++ cses/chessboard_and_queens.py
@@ -1,12 +1,12 @@
 boarda = [
-    input(),
-    input(),
-    input(),
-    input(),
-    input(),
-    input(),
-    input(),
-    input()
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2),
+    int(input().replace(".", "1").replace("*", "0"), 2)
 ]
 
 r8 = range(8)
@@ -20,7 +20,7 @@ def solver(board, put):
     while coor != None:
         r, c = coor
         count += solver(reserver(board.copy(), r, c), put + 1)
-        board[r] = board[r][:c] + "*" + board[r][c+1:]
+        board[r] &= 255 ^ 2**(7-c)
         coor = find_first_available(board)
     
     return count
@@ -29,24 +29,37 @@ def reserver(board, r, c):
     diag1 = lambda y: y + c - r
     diag2 = lambda y: - y + c + r
     
-    board[r] = "********"
+    board[r] = 0
     for j in r8:
         a = diag1(j)
         b = diag2(j)
         if a < 8 and a >= 0:
-            board[j] = board[j][:a] + "*" + board[j][a+1:]
+            board[j] &= 255 ^ 2**(7-a)
         if b < 8 and b >= 0:
-            board[j] = board[j][:b] + "*" + board[j][b+1:]
-        board[j] = board[j][:c] + "*" + board[j][c+1:]
+            board[j] &= 255 ^ 2**(7-b)
+        board[j] &= 255 ^ 2**(7-c)
                 
     return board
     
+    
 def find_first_available(board):
-    for i, r in enumerate(board):
-        c = r.find(".")
+    for i in r8:
+        c = plog2(board[i])
         if c != -1:
-            return i, c
+            return i, int(c)
             
     return None
+
+def plog2(d: int):
+    if d == 0:
+        return -1
+    
+    counter = 0
+    filterd = 128
+    while d & filterd == 0 and counter < 8:
+        filterd >>= 1
+        counter += 1
+        
+    return counter
 
 print(solver(boarda, 0))
