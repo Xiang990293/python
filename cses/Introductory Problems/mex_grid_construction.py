ba3dd2ee51de9662504327d87c355d4b5515523c
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-05 21:49:26 +0900

    resolve mex_grid_construction.py attempt 1

--- cses/Introductory Problems/mex_grid_construction.py
+++ cses/Introductory Problems/mex_grid_construction.py
@@ -1,23 +1,39 @@
 n = int(input())
 
 def solver(n):
-    if n == 0:
-        return [0]
+    if n == 1:
+        return [[0]]
 
     res = solver(n - 1)
 
-    start = res[0][-1]+1
+    start = res[0][-1] + 1
     temp = []
-    if n % 2 == 0:
+    if n % 2 == 1:
         for i in range(n - 1):
             res[i].append(start + i)
             temp.append(start + i)
 
-        res.append(temp.append(0))
+        temp.append(0)
+        res.append(temp)
 
         return res
 
     res[0].append(start)
     temp.append(start)
     for i in range(1, n - 1):
-        
+        all = set(range(2*n + 1))
+
+        exs = set(list(map(lambda x: x[i], res[:i]))).union(set(res[i]))
+        num = min(all.difference(exs))
+        res[i].append(num)
+        temp.append(num)
+
+    temp.append(0)
+    res.append(temp)
+
+    return res
+
+
+res = solver(n)
+for i in res:
+    print(str(i).replace("[","").replace("]","").replace(",",""))
