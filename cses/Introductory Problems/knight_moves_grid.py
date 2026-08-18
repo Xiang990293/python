f1b797b3b50dc08613d0dcd5baad106cd7caf153
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-18 21:26:58 +0900

    resolve knight_moves_grid.py attempt 7, TLE, without steped, can be done with board

--- cses/Introductory Problems/knight_moves_grid.py
+++ cses/Introductory Problems/knight_moves_grid.py
@@ -1,9 +1,9 @@
 from collections import deque
 
+
 n = int(input())
 
-board = [[-1 for j in range(n)] for i in range(n)]
-steped = set()
+board = [[-1] * n for i in range(n)]
 dirs_n = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (0, 1), (-1, 0)]
 dirs_f = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (0, -1), (1, 0)]
 dirs_o = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
@@ -26,7 +26,7 @@ def bfs(curl, level):
 
     dirs = []
 
-    dirs = dirs_d if cr == rr else (dirs_n if cr - rr == 1 else (dirs_f if cr - rr == 2 else dirs_o))
+    dirs = dirs_d.copy() if cr == rr else (dirs_n.copy() if cr - rr == 1 else (dirs_f.copy() if cr - rr == 2 else dirs_o.copy()))
 
     for i in dirs:
         d, e = i
@@ -35,22 +35,20 @@ def bfs(curl, level):
         if r > c or c > n - 1 or r < 0 or c < 0:
             continue
         
-        if (r, c) in steped:
+        if board[r][c] != -1:
             continue
 
         nextq.append((r, c))
         board[r][c] = level
         board[c][r] = level
-        steped.add((r, c))
     
     return nextq
 
 def solver():
     level = 1
+    board[0][0] = 0
     waitq = deque([(0, 0)])
     nextq = deque([])
-    board[0][0] = 0
-    steped.add((0, 0))
 
     while waitq:
         curl = waitq.popleft()
@@ -67,4 +65,4 @@ def solver():
 solver()
 
 for i in board:
-    print(str(i).replace("[", "").replace("]", "").replace(",", ""))
+    print(" ".join(map(str, i)))
