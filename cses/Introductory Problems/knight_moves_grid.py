c09994c22ab288369b844306ac72b1efcbe23252
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-18 21:07:33 +0900

    resolve knight_moves_grid.py attempt 6, TLE when n == 1000

--- cses/Introductory Problems/knight_moves_grid.py
+++ cses/Introductory Problems/knight_moves_grid.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = int(input())
 
 board = [[-1 for j in range(n)] for i in range(n)]
@@ -8,8 +10,16 @@ dirs_o = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)
 dirs_d = [(1, 2), (-1, 2), (-2, 1), (-2, -1)]
 
 def bfs(curl, level):
+    """
+        this return the non steped grid on the given "curl", 
+        and put the level value on the current grid.
+
+        curl contain three things:
+            1. rr: row_recent
+    """
+
     nextq = []
-    rr, cr, prev = curl
+    rr, cr = curl
 
     if cr < rr:
         return nextq
@@ -18,22 +28,17 @@ def bfs(curl, level):
 
     dirs = dirs_d if cr == rr else (dirs_n if cr - rr == 1 else (dirs_f if cr - rr == 2 else dirs_o))
 
-    # if prev != (0, 0):
-    #     r, c = prev
-    #     if (-r, -c) in dirs:
-    #         dirs.remove((-r, -c))
-
     for i in dirs:
         d, e = i
         r, c = rr+d, cr+e
         
-        if r > n - 1 or c > n - 1 or r < 0 or c < 0 or r > c:
+        if r > c or c > n - 1 or r < 0 or c < 0:
             continue
         
         if (r, c) in steped:
             continue
 
-        nextq.append((r, c, i))
+        nextq.append((r, c))
         board[r][c] = level
         board[c][r] = level
         steped.add((r, c))
@@ -42,20 +47,19 @@ def bfs(curl, level):
 
 def solver():
     level = 1
-    waitq = [(0, 0, (0, 0))]
-    nextq = []
+    waitq = deque([(0, 0)])
+    nextq = deque([])
     board[0][0] = 0
     steped.add((0, 0))
 
     while waitq:
-        curl = waitq.pop(0)
-        r, c, prev = curl
+        curl = waitq.popleft()
         
         nextq.extend(bfs(curl, level))
 
         if not waitq and nextq:
             waitq = nextq
-            nextq = []
+            nextq = deque([])
             level += 1
 
     return
