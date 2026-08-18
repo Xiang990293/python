21f83d55e56dc7be45fc6f353e4f3cf12a056e4b
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-08-18 21:40:49 +0900

    resolve knight_moves_grid.py attempt 9, TLE, rewrite bfs() in solve() to avoid memory reassignment

--- cses/Introductory Problems/knight_moves_grid.py
+++ cses/Introductory Problems/knight_moves_grid.py
@@ -1,68 +1,42 @@
 from collections import deque
 
-
-n = int(input())
-
-board = [[-1] * n for i in range(n)]
 dirs_n = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (0, 1), (-1, 0)]
 dirs_f = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (0, -1), (1, 0)]
 dirs_o = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
 dirs_d = [(1, 2), (-1, 2), (-2, 1), (-2, -1)]
 
-def bfs(curl, level):
-    """
-        this return the non steped grid on the given "curl", 
-        and put the level value on the current grid.
-
-        curl contain three things:
-            1. rr: row_recent
-    """
-
-    nextq = []
-    rr, cr = curl
-
-    if cr < rr:
-        return nextq
-
-    dirs = []
-
-    dirs = dirs_d if cr == rr else (dirs_n if cr - rr == 1 else (dirs_f if cr - rr == 2 else dirs_o))
-
-    for i in dirs:
-        d, e = i
-        r, c = rr+d, cr+e
-        
-        if r > c or c > n - 1 or r < 0 or c < 0:
-            continue
-        
-        if board[r][c] != -1:
-            continue
-
-        nextq.append((r, c))
-        board[r][c] = level
-        board[c][r] = level
-    
-    return nextq
-
 def solver():
-    level = 1
-    board[0][0] = 0
     waitq = deque([(0, 0)])
-    nextq = deque([])
-
     while waitq:
-        curl = waitq.popleft()
+        rr, cr = waitq.popleft()
+        level = board[rr][cr] + 1
         
-        nextq.extend(bfs(curl, level))
+        if cr < rr:
+            continue
 
-        if not waitq and nextq:
-            waitq = nextq
-            nextq = deque([])
-            level += 1
+        dirs = []
+        
+        dirs = dirs_d if cr == rr else (dirs_n if cr - rr == 1 else (dirs_f if cr - rr == 2 else dirs_o))
+    
+        for i in dirs:
+            d, e = i
+            r, c = rr+d, cr+e
+            
+            if r > c or c > n - 1 or r < 0 or c < 0:
+                continue
+            
+            if board[r][c] != -1:
+                continue
+
+            waitq.append((r, c))
+            board[r][c] = level
+            board[c][r] = level
 
     return
 
+n = int(input())
+board = [[-1] * n for i in range(n)]
+board[0][0] = 0
 solver()
-
 for i in board:
     print(" ".join(map(str, i)))
